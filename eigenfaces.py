74b91caa43847f3e979e529aeef8c06af5804a7e
vagrant <vagrant@vagrant.vm>
2014-11-15 08:21:48 +0000

    update

--- eigenfaces.py
+++ eigenfaces.py
@@ -1,41 +1,30 @@
 #!/usr/bin/env python
 import numpy as np
 
-class Faces(object):
-    def __init__(self,matrix):
-        self.matrix = matrix
-        self.matrix = self.matrix.reshape(self.height * self.width,1)
-
 class EigenFaces(object):
     def __init__(self,faces):
         self.matrix = []
         for face in faces:
             self.matrix.append(face)
     
-    def calculatingEigenVector(self):
+    def eigenvector(self):
         covariance = np.dot(np.transpose(self.matrix), self.matrix)
         eigenVector = np.linalg.eig(covariance)
         return eigenVector
     
-    def calculatingAverage(self):
-        result = []
-        for x in range(0,len(self.matrix)):
-            print self.matrix[x]
-            a = 0
-            for y in range(0,len(self.matrix[x])):
-                a =  a +  self.matrix[y][x]
-            a = a / (len(self.matrix[x]) + 1)
-            print a
-            result.append(a)
-        return result
+    def average(self):
+        result = self.matrix[0]
+        for x in range(1,len(self.matrix)):
+            result = np.add(result,self.matrix[x])
+        return result/float(len(self.matrix))
 
 def main():
     face1 = np.arange(20)
-    face2 = np.arange(20)
+    face2 = np.arange(1,40,2)
     a = [face1,face2]
     eigenface = EigenFaces(a)
-    print a
-    print eigenface.calculatingAverage()
+    print eigenface.eigenvector()
+    print eigenface.average()
 
 
 if __name__ == '__main__':
