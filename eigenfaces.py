f1722a64add889008a8d98f3d7a01542c7258dc5
vagrant <vagrant@vagrant.vm>
2014-11-15 08:46:22 +0000

    sleep time

--- eigenfaces.py
+++ eigenfaces.py
@@ -6,26 +6,31 @@ class EigenFaces(object):
         self.matrix = []
         for face in faces:
             self.matrix.append(face)
-    
-    def eigenvector(self):
-        covariance = np.dot(np.transpose(self.matrix), self.matrix)
-        eigenVector = np.linalg.eig(covariance)
-        return eigenVector
-    
-    def average(self):
-        result = self.matrix[0]
-        for x in range(1,len(self.matrix)):
-            result = np.add(result,self.matrix[x])
-        return result/float(len(self.matrix))
 
+        covariance = np.dot(np.transpose(self.matrix), self.matrix)
+        self.eigenvectors = np.linalg.eig(covariance)
+        self.eigenvalues = self.eigenvectors[0]
+        self.eigenvectors = self.eigenvectors[1]
+        self.average = self.matrix[0]
+        for x in range(1,len(self.matrix)):
+            self.average = np.add(self.average,self.matrix[x])
+        self.average = self.average/float(len(self.matrix))
+
+ 
+ 
+def weightvector(self,face): 
+    return np.dot(np.transpose(eigenvectors),np.subtract(face,self.average))
+
+
+    
 def main():
     face1 = np.arange(20)
     face2 = np.arange(1,40,2)
     a = [face1,face2]
     eigenface = EigenFaces(a)
-    print eigenface.eigenvector()
-    print eigenface.average()
-
+    print eigenface.eigenvectors
+    print eigenface.eigenvalues
+    print eigenface.average
 
 if __name__ == '__main__':
     main()
